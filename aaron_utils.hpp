5235ad935af5e9a143ae413d87b7e692d8b2d6d8
Aaron McDaid <aaronmcdaid@erdos.ucd.ie>
2011-03-15 16:31:47 +0000

    aaron_utils.hpp: PP4 and others

--- aaron_utils.hpp
+++ aaron_utils.hpp
@@ -28,14 +28,18 @@ enum {FALSE=0, TRUE=1};
 #define PP(x) cout << #x << ":" << x << endl
 #define PPt(x) cout << #x << ":" << x << '\t'
 #define PPnn(x) cout << #x << ":" << x
-#define PP2(x,y) cout << #x << ',' << #y << ":\t" << x << ',' << y << endl
-#define PP3(x,y,z) cout << #x << ',' << #y << ',' << #z << ":\t" << x << ',' << y << ',' << z << endl
+#define PP2(x,y) cout << #x << ',' << #y << ":\t" << x << " , " << y << endl
+#define PP3(x,y,z)             cout << #x << ',' << #y << ',' << #z                              << ":\t" << x << " , " << y << " , " << z << endl
+#define PP4(x,y,z,w)           cout << #x << ',' << #y << ',' << #z << ',' << #w                 << ":\t" << x << " , " << y << " , " << z << " , " << w << endl
 #define PPLg(x) Pn("%s:%20.11Lg", #x, x)
 std::string thousandsSeparated(uint64 x);
 #define PPdec(x) cout << #x << ":" << thousandsSeparated(x) << endl
 #define PPhex(x) cout << #x << ":" << std::hex << std::setw(20) << x << std::dec << endl
 #define Print(x)  P("%s", show(x).c_str())
 #define Printn(x) P("%s\n", show(x).c_str())
+#define v1(...) do { if(global_verbose_flag)  PP(__VA_ARGS__); } while(0)
+#define v2(...) do { if(global_verbose_flag) PP2(__VA_ARGS__); } while(0)
+#define v3(...) do { if(global_verbose_flag) PP3(__VA_ARGS__); } while(0)
 enum {
 	EXIT_UNSPECIFIED=1  // other (unspecified) error. Try to avoid using this
 	, EXIT_FILEOPEN     // child process failed because it couldn't open the requested file
