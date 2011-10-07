ebeb418448f9512f1b8cf7078ddfa053e0547497
Aaron McDaid <aaronmcdaid@gmail.com>
2011-10-07 20:58:39 +0100

    labels: I'm pretty happy with it now. It's a bit weird with Hugo I think.

--- labels.cpp
+++ labels.cpp
@@ -9,7 +9,7 @@
 #include "macros.hpp"
 using namespace std;
 
-typedef pair<int, vector<int> > kz_t;
+typedef pair<int, pair<int,vector<int> > > kz_t; // K, (burnt)iteration, z
 
 struct sort_on_pair_first {
 	bool operator() (const kz_t &l, const kz_t &r) const {
@@ -66,8 +66,8 @@ const pair<int64_t, vector<int> > find_best_labellings(const int K, const vector
 	vector<int> already_taken(K, -1);
 	vector< vector<int> > best_relabellings_so_far;
 	recurse(K, k_by_k, 0, already_taken, 0, best_score_so_far, best_relabellings_so_far);
-	PP(best_relabellings_so_far.size());
-	PP(best_score_so_far);
+	if(best_relabellings_so_far.size()>1)
+		PP(best_relabellings_so_far.size());
 	assert(1<=best_relabellings_so_far.size());
 	const int offset = drand48() * best_relabellings_so_far.size();
 	return make_pair(best_score_so_far,  best_relabellings_so_far.at(offset));
@@ -77,6 +77,7 @@ int main() {
 	string line;
 	int N = -1;
 	vector<kz_t> kzs;
+	int line_no = 0;
 	while(getline(cin, line)) {
 		istringstream fields(line);
 		int K;
@@ -93,7 +94,8 @@ int main() {
 		if(N == -1)
 			N = z.size();
 		assert(N == int(z.size()));
-		kzs.push_back(make_pair(K, z));
+		kzs.push_back(make_pair(K, make_pair(line_no, z)));
+		++ line_no;
 	}
 	PP2(N, kzs.size());
 
@@ -105,7 +107,7 @@ int main() {
 	int num_kz_so_far = 0;
 	For(kz, kzs) {
 		const int K = kz->first;
-		PP3(num_kz_so_far, K, kz->second.at(0));
+		PP4(num_kz_so_far, K, kz->second.first, kz->second.second.at(0));
 		// decide which relabelling to use:
 		// - try an optimisitc relabelling first
 		// - else fail
@@ -123,7 +125,7 @@ int main() {
 			vector< vector<int64_t> > k_by_k(K, vector<int64_t>(K,0));
 			// k_by_k is like a summary of node_k_counts, but where the nodes are summed into their clusters.
 			for(int n=0; n<74; n++) {
-				const int z_i = kz->second.at(n);
+				const int z_i = kz->second.second.at(n);
 				for(int k=0; k<K; k++) {
 					k_by_k.at(z_i).at(k) += node_k_counts[make_pair(n,k)];
 					// PP3(z_i, k, k_by_k.at(z_i).at(k));
@@ -151,14 +153,20 @@ int main() {
 			}
 			PP(best_score_so_far);
 			{ // print the relabelled form
+				vector<int> cluster_sizes(K);
 				for(int n=0; n<N; n++) {
-					const int z_i = kz->second.at(n);
+					const int z_i = kz->second.second.at(n);
 					const int rel_z_i = one_of_the_best_labellings.second.at(z_i);
 					cout << ' ' << rel_z_i;
+					++ cluster_sizes.at(rel_z_i);
+				}
+				cout << '\t';
+				For(sz, cluster_sizes) {
+					cout << ' ' << *sz;
 				}
 				cout << endl;
 			}
-			assert(best_score_so_far == 0);
+			// assert(best_score_so_far == 0);
 			For(x, one_of_the_best_labellings.second)
 				relabelling.push_back(*x);
 		}
@@ -167,14 +175,14 @@ int main() {
 		vector<int> relabelled_z;
 		int verification_score = 0;
 		for(int n=0; n<N; n++) {
-			const int z_i = kz->second.at(n);
+			const int z_i = kz->second.second.at(n);
 			const int rel_z_i = relabelling.at(z_i);
 			verification_score += node_k_counts[make_pair(n, rel_z_i)];
 		}
 		// PP3(K,verification_score, num_kz_so_far);
 		// assert(verification_score >= 74 * num_kz_so_far);
 		for(int n=0; n<N; n++) {
-			relabelled_z.push_back(relabelling.at(kz->second.at(n)));
+			relabelled_z.push_back(relabelling.at(kz->second.second.at(n)));
 		}
 		for(int n=0; n<N; n++) {
 			const int relabelled_z_i = relabelled_z.at(n);
