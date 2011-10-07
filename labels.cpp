b62a36c07fdda643ad1a75dd8702178e88d3a287
Aaron McDaid <aaronmcdaid@gmail.com>
2011-10-07 17:02:14 +0100

    Labels: (stable) Sort by K

--- labels.cpp
+++ labels.cpp
@@ -2,31 +2,46 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <algorithm>
 #include <cassert>
 #include "macros.hpp"
 using namespace std;
 
+typedef pair<int, vector<int> > kz_t;
+
+struct sort_on_pair_first {
+	bool operator() (const kz_t &l, const kz_t &r) const {
+		return l.first < r.first;
+	}
+};
+
 int main() {
 	string line;
 	int N = -1;
+	vector<kz_t> kzs;
 	while(getline(cin, line)) {
-		PP(line);
-		For(i, line) {
-			if(*i==':' || *i == ',')
-				*i='\n';
-		}
 		istringstream fields(line);
 		int K;
+		char sink;
 		fields >> K;
-		PP(K);
+		fields >> sink;
 		vector<int> z;
 		int z_i;
 		while(fields >> z_i) {
+			fields >> sink;
 			z.push_back(z_i);
 		}
-		PP(z.size());
+		assert(fields.eof());
 		if(N == -1)
 			N = z.size();
 		assert(N == int(z.size()));
+		kzs.push_back(make_pair(K, z));
+	}
+	PP2(N, kzs.size());
+
+	stable_sort(kzs.begin(), kzs.end(), sort_on_pair_first());
+
+	For(kz, kzs) {
+		PP2(kz->first, kz->second.at(0));
 	}
 }
