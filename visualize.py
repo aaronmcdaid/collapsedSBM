2a41ae34bce7f5b159e7f0ca7a773ccf77593f93
Aaron McDaid <aaronmcdaid@gmail.com>
2012-03-27 14:51:51 +0100

    visualize.py: a plot() function.

--- visualize.py
+++ visualize.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 import collections
+import pylab
+import random
+import networkx as nx
 
 def average_key_in_dict(d):
 	xs = [k*v for k,v in d.iteritems()]
@@ -15,7 +18,7 @@ def process_z_unswitched():
 	z_unswitched = open('z.unswitched')
 	header = z_unswitched.readline()
 	N = int(header.strip().split(':')[1].split(',')[0])
-	print N
+	print "N=", N
 	num_lines = 0
 	counts = [collections.defaultdict(int) for n in range(N)]
 	for line in z_unswitched:
@@ -40,6 +43,23 @@ def main():
 	all_data = process_z_unswitched()
 	for a in all_data:
 		print a
+
+def plot(all_data):
+	G=nx.read_edgelist('../../edge_list.txt', nodetype=int, delimiter='\t', create_using=nx.DiGraph())
+	N=G.order()
+	mat=nx.convert.to_numpy_matrix(G, nodelist=[a[0] for a in all_data])
+	#mat=[[random.randint(0,1) for i in range(N)] for j in range(N)]
+	pylab.cla(); pylab.imshow(pylab.asarray(1-mat), cmap=pylab.cm.gray, interpolation='nearest')
+
+	current_cluster = all_data[0][1]
+	for i, x in enumerate(all_data):
+		if i==0:
+			continue
+		if current_cluster != x[1]:
+			print x[1]
+			current_cluster = x[1]
+			pylab.axhline(y=i-0.55,color='red',linewidth=2)
+			pylab.axvline(x=i-0.45,color='red',linewidth=2)
 
 
 if __name__ == "__main__":
