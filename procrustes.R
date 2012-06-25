2ef585ec471fcb9339ef95291be133214a196b74
Aaron McDaid <aaronmcdaid@gmail.com>
2012-06-25 12:11:14 +0100

    procrustes: nicer colours, and choice of symbol (pch=8)

--- procrustes.R
+++ procrustes.R
@@ -34,20 +34,20 @@ set.seed(2012)
 
 plot(c(),c(), xlim=c(-8,8), ylim=c(-8,8))
 
-running_total = all_data.ts[[1]]
-for(firstrun in 2:timepoints) {
-# for(firstrun in 2:10) {
-	print(c('firstrun ', firstrun))
-	source = running_total / (firstrun-1)
-	print(source)
+running_total = 0 * all_data.ts[[1]]
+for(firstrun in 1:timepoints) {
+# for(firstrun in 1:1) {
+	# print(c('firstrun ', firstrun))
+	if(firstrun > 1) {
+		source = running_total / (firstrun-1)
+		# print(source)
 
-	print(sum((all_data.ts[[firstrun]] - source)**2))
-
-	newversion = procrustes(all_data.ts[[firstrun]] , source)$X.new
-	all_data.ts[[firstrun]] <- newversion
-	points(all_data.ts[[firstrun]], col=c('#ff000050','#00ff0050','#0000ff50','#ffff0050'))
-
-	print(sum((all_data.ts[[firstrun]] - source)**2))
-
+		#print(sum((all_data.ts[[firstrun]] - source)**2))
+		newversion = procrustes(all_data.ts[[firstrun]] , source)$X.new
+		all_data.ts[[firstrun]] <- newversion
+		# print(sum((all_data.ts[[firstrun]] - source)**2))
+	}
+	transparency=function(colour) { return(paste(colour,'',sep='')); }
+	points(all_data.ts[[firstrun]], col=c(transparency('#ff0000'),transparency('#00cc00'),transparency('#0000ff'),transparency('#aaaa00')), pch=8, cex=0.1)
 	running_total = running_total + all_data.ts[[firstrun]]
 }
